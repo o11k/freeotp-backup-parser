87aa891c361c1c91b9a117350a70e3cf8b347502
Ori Levenglick <61731312+o11k@users.noreply.github.com>
2025-11-19 18:44:50 -0600

    Parse file in JS

--- componenets.jsx
+++ componenets.jsx
@@ -56,8 +56,8 @@ function AppComponent() {
             if (file) {
                 const bytes = await readFile(file);
                 await cheerpOSAddStringFile("/str/externalBackup.xml", bytes);
-                const fileDataStr = await window.JavaClass.parseBackupFile("/str/externalBackup.xml");
-                currFileData = JSON.parse(fileDataStr);
+                const jsFileData = parseBackupFile(bytes);
+                currFileData = parseBackupFile(bytes);
                 setFileData(currFileData);
             }
         }
@@ -146,8 +146,10 @@ function AppComponent() {
                     <tbody>
                         {fileData.tokens.map((t, i) => {
                             return <tr key={btoa(t.key.mCipherText.map(c => String.fromCharCode((c+255)%255)).join(""))}>
-                                <td>{otpUris === null ? "🔒" : <QR text={otpUris[i]} width={200} height={200} />}</td>
-                                <td>{t.token.issuerInt ?? t.token.issuerExt ?? "<unknown>"}</td>
+                                <td style={{padding: 30}}>
+                                    {otpUris === null ? "🔒" : <QR text={otpUris[i]} width={200} height={200} />}
+                                </td>
+                                <td>{t.token.issuerExt ?? t.token.issuerInt ?? "<unknown>"}</td>
                                 <td>{t.token.label}</td>
                             </tr>
                         })}
@@ -165,6 +167,114 @@ function QR({ text, width, height }) {
     }, [text, width, height])
 
     return <div ref={handleRef}></div>
+}
+
+function parseBackupFile(/** @type {Uint8Array} */ data) {
+    let index = 0;
+
+    const read = (numBytes, exact=true) => {
+        if (exact && index + numBytes > data.byteLength) {
+            throw Error("Unexpected EOF");
+        }
+        const result = data.slice(index, index+numBytes);
+        index += numBytes;
+        index = Math.min(index, data.byteLength);
+        return result;
+    }
+
+    const readInteger = (numBytes, signed=false) => {
+        const bytes = read(numBytes);
+
+        let result = 0n;
+        for (const byte of bytes) {
+            result = (result << 8n) + BigInt(byte);
+        }
+
+        // Signed int
+        const modulus = 1n << BigInt(bytes.byteLength * 8);
+        const signBit = bytes[0] >> (8-1)
+        if (signed && signBit) {
+            result = result - modulus;
+        }
+
+        return result;
+    }
+
+    const readUtf = () => {
+        const length = readInteger(2);
+        const bytes = read(Number(length));
+        return new TextDecoder().decode(bytes);
+    }
+
+    const expect = (expected, actual, info="") => {
+        let same;
+        if (expected instanceof Uint8Array && actual instanceof Uint8Array) {
+            same = expected.byteLength === actual.byteLength && expected.every((b, i) => b === actual[i]);
+        } else {
+            if (typeof expected === "number") expected = BigInt(expected);
+            if (typeof actual === "number") actual = BigInt(actual);
+            same = expected === actual;
+        }
+        if (!same) {
+            if (info) info += " ";
+            throw Error(`Error while parsing file: expected ${info}${expected}, found ${actual}`);
+        }
+    }
+
+    // Header
+    expect(0xaced, readInteger(2), "STREAM_MAGIC");
+    expect(5, readInteger(2), "STREAM_VERSION");
+
+    expect(0x73, readInteger(1), "TC_OBJECT");
+
+    // Class Desc
+    expect(0x72, readInteger(1), "TC_CLASSDESC");
+    expect("java.util.HashMap", readUtf());
+    expect(0x0507dac1c31660d1n, readInteger(8), "serialVersionUID");
+    expect(0x03, readInteger(1), "classDescFlags");
+    expect(2, readInteger(2), "fieldCount");
+    expect(0x46, readInteger(1), "float indicator");
+    expect("loadFactor", readUtf());
+    expect(0x49, readInteger(1), "int indicator");
+    expect("threshold", readUtf());
+    expect(0x78, readInteger(1), "TC_ENDBLOCKDATA");
+    expect(0x70, readInteger(1), "TC_NULL");
+
+    // Class Data
+    read(4);  // loadFactor
+    read(4);  // threshold
+
+    expect(0x77, readInteger(1), "TC_BLOCKDATA");
+    expect(8, readInteger(1), "block data length");
+    const _hashMapCapacity = readInteger(4);
+    const hashMapSize = readInteger(4);
+
+    const result = {};
+    const tokensMap = new Map();
+
+    for (let i=0; i<hashMapSize; i++) {
+        expect(0x74, readInteger(1), "TC_STRING");
+        const key = readUtf();
+        expect(0x74, readInteger(1), "TC_STRING");
+        const value = readUtf();
+
+        if (key === "masterKey") {
+            result.masterKey = JSON.parse(value);
+        } else if (key.endsWith("-token")) {
+            const uuid = key.slice(0, -"-token".length);
+            if (!tokensMap.has(uuid)) tokensMap.set(uuid, {});
+            tokensMap.get(uuid).token = value;
+        } else {
+            const uuid = key;
+            if (!tokensMap.has(uuid)) tokensMap.set(uuid, {});
+            tokensMap.get(uuid).key = value;
+        }
+    }
+    result.tokens = [...tokensMap.values()];
+
+    expect(0x78, readInteger(1), "TC_ENDBLOCKDATA");
+
+    return result;
 }
 
 ReactDOM.createRoot(document.getElementById("freeotp-root")).render(<AppComponent />);
